0f210e1dd0dc766f975eb6fef0d47c1b55e0a120
Robert Montilla <robertmontilla94@gmail.com>
2020-09-22 19:01:35 -0400

    Modifica el server.

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import sirv from 'sirv';
-import polka from 'polka';
+import express from 'express'
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import api from './api'
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
+express()
+	.use('/api', api)
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
